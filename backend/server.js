2b23754cd337a9649a2ad634053e6be4f899ebbc
BlitzBlitz <klementbinaj@gmail.com>
2022-08-17 12:37:29 +0200

    connected to DB

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,16 @@ app.use(express.json());
 // routes
 app.use("/api/bikes", bikeRoutes);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("Server is listening on port ", process.env.PORT);
-});
+//mongooes connection
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Server is listening on port ", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
