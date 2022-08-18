54655d3e0f702b242f831e6d609d23527b02d307
BlitzBlitz <klementbinaj@gmail.com>
2022-08-18 23:42:00 +0200

    users CRUD

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -7,6 +7,56 @@ const createToken = (user) => {
     expiresIn: "3d",
   });
 };
+
+//get all users
+module.exports.getAllUsers = (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Error getting users",
+      });
+    }
+    return res.status(200).json({ users });
+  });
+};
+
+//get user by id
+module.exports.getUserById = (req, res) => {
+  User.findById(req.params.id, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Error getting user",
+      });
+    }
+    return res.status(200).json({ user });
+  });
+};
+
+//update user by id
+module.exports.updateUserById = (req, res) => {
+  User.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Error updating user",
+      });
+    }
+    return res.status(200).json({ user });
+  });
+};
+
+//delete user by id
+module.exports.deleteUserById = (req, res) => {
+  User.findByIdAndDelete(req.params.id, (err, user) => {
+    if (err) {
+      return res.status(500).json({
+        message: "Error deleting user",
+      });
+    }
+    return res.status(200).json({ user });
+  });
+};
+
+//login user
 module.exports.login = (req, res) => {
   const { email, password } = req.body;
   User.login(email, password)
