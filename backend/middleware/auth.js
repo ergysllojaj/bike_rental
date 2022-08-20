3d3e5630f6adf9d8cc7e624698e1629bd1ca195e
BlitzBlitz <klementbinaj@gmail.com>
2022-08-20 16:14:42 +0200

    disable auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,27 +2,28 @@ const jwt = require("jsonwebtoken");
 
 module.exports.isAuthAs = (roles) => {
   return (req, res, next) => {
-    let token = req.headers.authorization;
-    if (!token) {
-      return res.status(401).json({
-        message: "No token provided",
-      });
-    }
-    token = token.split(" ")[1];
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(401).json({
-          message: "Invalid token",
-        });
-      }
-      if (roles.includes(decoded.role)) {
-        res.userId = decoded.id;
-        next();
-      } else {
-        return res.status(401).json({
-          message: "You don't have permission to do this action",
-        });
-      }
-    });
+    // let token = req.headers.authorization;
+    // if (!token) {
+    //   return res.status(401).json({
+    //     message: "No token provided",
+    //   });
+    // }
+    // token = token.split(" ")[1];
+    // jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    //   if (err) {
+    //     return res.status(401).json({
+    //       message: "Invalid token",
+    //     });
+    //   }
+    //   if (roles.includes(decoded.role)) {
+    //     res.userId = decoded.id;
+    //     next();
+    //   } else {
+    //     return res.status(401).json({
+    //       message: "You don't have permission to do this action",
+    //     });
+    //   }
+    // });
+    next();
   };
 };
